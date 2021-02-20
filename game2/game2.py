3008a522351b297a160f653320104adf60ca2d4c
Theodore Thayib <theodore.thayib@gmail.com>
2021-02-19 23:21:27 -0600

    Blackjack game works

--- game2/game2.py
+++ game2/game2.py
@@ -36,7 +36,10 @@ class Hand:
         self.aceElevenValue = aceElevenValue
         self.hasAce = hasAce
 
-    def add_card(self, cardValue):
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def add_card_value(self, cardValue):
         modcardvalue = ((cardValue - 1) % 13) + 1
         modcardvalue = 10 if (modcardvalue > 10) else modcardvalue
         if not self.hasAce and modcardvalue == 1:
@@ -50,18 +53,51 @@ class Hand:
     def hand_status(self):
         if self.aceOneValue > 21:
             return 'Bust'
-        elif self.aceElevenValue == 21 or self.aceOneValue == 21:
-            return 'Blackjack'
         else:
             return 'Playable'
 
+    def reset(self):
+        self.cards = []
+        self.aceOneValue = 0
+        self.aceElevenValue = 0
+        self.hasAce = False
 
-def new_game(player, computer):
+
+def new_game(p1, c1):
+    global card_deck
     card_deck = deck_of_cards
     # player_hand = []
     # computer_hand = []
-    player = Hand([], 0, 0, False)
-    computer = Hand([], 0, 0, False)
+    p1.reset()
+    c1.reset()
+
+
+def print_player_cards(cards):
+    for card in cards:
+        cardstr = ''
+        cardval = ((card - 1) % 13) + 1
+        cardsuite = int((card - 1) / 13)
+        if cardval == 11:
+            cardstr = 'Jack'
+        elif cardval == 12:
+            cardstr = 'Queen'
+        elif cardval == 13:
+            cardstr = 'King'
+        elif cardval == 1:
+            cardstr = 'Ace'
+        else:
+            cardstr = str(cardval)
+        cardstr += ' Of '
+        if cardsuite == 0:
+            cardstr += 'Clubs'
+        elif cardsuite == 1:
+            cardstr += 'Diamonds'
+        elif cardsuite == 2:
+            cardstr += 'Spades'
+        else:
+            cardstr += 'Hearts'
+        print(cardstr)
+    # print(cards)
 
 
 def load_game(save_location):
@@ -74,7 +110,112 @@ def save_game():
     # TODO save game to file
 
 
-# def player_turn():
+def player_turn(player):
+    card = input('Hit (H) or Stand (S)?')
+    if card == 'H':
+        print('HIT')
+        hit(player)
+        return True
+    elif card == 'S':
+        print('STAND')
+        return False
+
+
+def hit(player):
+    cardDrawn = random.randrange(0, len(card_deck), 1)
+    cardval = card_deck[cardDrawn]
+    player.add_card(cardval)
+    del card_deck[cardDrawn]
+    player.add_card_value(cardval)
+
+
+def game_instance(player, computer):
+    doplay = True
+    while doplay:
+        playergo = True
+        computergo = True
+        player1val = 0
+        computerval = 0
+        status = 0
+
+        # cardDrawn = random.randrange(0, len(card_deck), 1)
+        # cardval = card_deck[cardDrawn]
+        # computer.add_card(cardval)
+        # del card_deck[cardDrawn]
+        # computer.add_card_value(cardval)
+
+        hit(player)
+        hit(computer)
+        hit(player)
+
+        print('The computer has a face-up card of:')
+        print_player_cards(computer.cards)
+
+        print('You have cards of:')
+        print_player_cards(player.cards)
+
+        hit(computer)
+        computerval = computer.aceElevenValue
+
+        if player.aceElevenValue == 21:
+            print('You have a blackjack!')
+            playergo = False
+            player1val = -2
+            if computerval == 21:
+                print('The computer also has a blackjack. This hand will be pushed.')
+                status = 1
+            else:
+                print('The computer does not have a blackjack. You win!')
+
+        while playergo:
+            playergo = player_turn(player)
+            # print(player.aceOneValue)
+            # print(player.aceElevenValue)
+            print("Your current hand consists of: ")
+            print_player_cards(player.cards)
+            curstatus = player.hand_status()
+            if curstatus == 'Bust':
+                # print('Bust. Better luck next time!')
+                player1val = -1
+                break
+            else:
+                player1val = player.aceElevenValue if player.aceElevenValue < 21 else player.aceOneValue
+        # print(player1val)
+        while player1val >= 0 and computerval < 17:
+            hit(computer)
+            computerval = computer.aceElevenValue
+            print("The computer's current hand consists of:")
+            print_player_cards(computer.cards)
+            if computerval > 21:
+                print('The computer busts! You win!')
+                break
+        if player1val > -2:
+            if player1val > computerval:
+                print('You win! Congratulations!')
+            elif player1val == computerval:
+                print('You and the computer have the same hand total! This hand will be pushed')
+                status = 1
+            elif player1val == -1:
+                print('You have busted. Better luck next time!')
+            else:
+                print('The computer has the better hand. Better luck next time!')
+        # print(player1val)
+        # print(computerval)
+
+        while status != 1:
+            goagain = input('Would you like to play again? (Y/N)')
+            if goagain == 'Y':
+                print('Playing again. Good luck!')
+                print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+                new_game(player, computer)
+                break
+            elif goagain == 'N':
+                print('See you next time!')
+                doplay = False
+                break
+            else:
+                print('Please input Y or N')
+
 
 
 def hand_total(hand):
@@ -94,11 +235,9 @@ def hand_total(hand):
 def main():
     print('test')
     player = Hand([], 0, 0, False)
-    player.add_card(3)
-    print(player.aceOneValue)
     computer = Hand([], 0, 0, False)
     new_game(player, computer)
-    print(player.aceOneValue)
+    game_instance(player, computer)
     # print(hand_total([26, 14]))
 
 
