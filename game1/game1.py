9b73725c3896875ab67e51a14f262621cb08fd69
Kim Phu <kimgorawr@gmail.com>
2021-02-21 23:49:14 -0600

    Auto stash before merge of "main" and "origin/main"

--- game1/game1.py
+++ game1/game1.py
@@ -1,5 +1,5 @@
 '''
-1. Replace this block comment with a useful description of what this file and what your game is
+1. tic tac toe pog
 
 Make the game start by running this file.
 
@@ -15,12 +15,11 @@ Make the game start by running this file.
     d. Example: tictactoe winner is decided based on current state of game and the player's input
     e. Tictactoe game board can be stored in a list, and the player input could be verified by making sure input is an open space
     
-Author:
+Author: blub blub blub
 '''
 
 import random
 
-# game_state = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 game_state = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 def foo(game_state, user_choice, computer_choice):
@@ -54,6 +53,41 @@ def show_game_state():
     print(game_state[1][0] + '|' + game_state[1][1] + '|' + game_state[1][2])
     print('------')
     print(game_state[2][0] + '|' + game_state[2][1] + '|' + game_state[2][2])
+
+def edit_game_state(xox, loc):
+    game_state[int(loc[0])][int(loc[1])] = xox
+
+def get_user_input(xox_input):
+    loc_input = get_loc_input()
+    edit_game_state(xox_input, loc_input)
+
+def vs_computer_input():
+    loc_input = get_loc_input()
+    edit_game_state("X", loc_input)
+
+def get_loc_input():
+    while True:
+        loc_input = []
+        while True:
+            loc1 = input("Please enter row number between 0-2.")
+            if loc1 == "0" or loc1 == "1" or loc1 == "2":
+                break
+            else:
+                print("You did not enter a number between 0-2, try again.")
+        loc_input.append(loc1)
+        while True:
+            loc2 = input("Please enter row number between 0-2.")
+            if loc2 == "0" or loc2 == "1" or loc2 == "2":
+                break
+            else:
+                print("You did not enter a number between 0-2, try again.")
+        loc_input.append(loc2)
+        if game_state[int(loc_input[0])][int(loc_input[1])] == " ":
+            break
+        else:
+            print("That square is occupied. ):<")
+
+    return loc_input
 
 def win_lose():
     if game_state[0][0] == game_state[1][0] and game_state[0][0] == game_state[2][0] and game_state[0][0] != " ":
@@ -92,25 +126,77 @@ def win_lose():
     temp = 1
     for i in range(len(game_state)):
         for j in range(len(game_state)):
-            if game_state[i][j] != " ":
+            if game_state[i][j] == " ":
                 temp = 0
-    if temp == 0:
+    if temp == 1:
         return 3
 
     return 0
 
+def computer_move():
+    possible_moves = []
+    for i in range(len(game_state)):
+        for j in range(len(game_state)):
+            if game_state[i][j] == " ":
+                possible_moves.append(i * 3 + j)
+    rand_move = random.randrange(0, len(possible_moves), 1)
+    loc_move = [int(possible_moves[rand_move] / 3), int(possible_moves[rand_move] % 3)]
+    edit_game_state("O", loc_move)
 
+def vs_computer():
+    new_game()
+
+    play = 0
+
+    while play == 0:
+        vs_computer_input()
+        show_game_state()
+        play = win_lose()
+        print("\n")
+        if play == 0:
+            computer_move()
+            show_game_state()
+            play = win_lose()
+    if play == 1:
+        print("Congratulations! You've won! :D")
+    elif play == 2:
+        print("You've lost.")
+    elif play == 3:
+        print("Draw!")
+
+
+def vs_player():
+    new_game()
+
+    play = 0
+
+    while play == 0:
+        get_user_input("X")
+        show_game_state()
+        play = win_lose()
+        print("\n")
+        if play == 0:
+            get_user_input("O")
+            show_game_state()
+            play = win_lose()
+    if play == 1:
+        print("Congratulations, Player 1 has won! :D")
+    elif play == 2:
+        print("Congratulations, Player 2 has won! :D")
+    elif play == 3:
+        print("Draw!")
 
 def main():
-    '''You don't have to use this skeleton code'''
+    while True:
+        user_input = input("Would you like to play against a computer (C), a player (P), or quit (Q)?").upper()
+        if user_input == "C":
+            vs_computer()
+        elif user_input == "P":
+            vs_player()
+        elif user_input == "Q":
+            print("Bye.")
+            break
 
-    #TODO: implement
-    # game_state = new_game()
-
-    # get_user_input()
-    show_game_state()
-
-    print(win_lose())
 
     # while True:
     #     #TODO: prompt user to make a selection or call a function to retrieve it
@@ -120,7 +206,7 @@ def main():
     #     #TODO: select computer's choice or call a function to retrieve it
     #     computer_choice = None
     #
-    #     #TODO: decide and dispaly the results
+    #     #TODO: decide and display the results
     #     game_result = foo(game_state, user_choice, computer_choice)
     #
     #     #TODO: implement quit or start new game condition
